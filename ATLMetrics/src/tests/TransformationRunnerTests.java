44890fee95db9b140618e9f2b19afb8b4b698f2b
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-08-20 15:50:24 +0200

    test class TransformationRunner

---
+++ ATLMetrics/src/tests/TransformationRunnerTests.java
@@ -0,0 +1,52 @@
+package tests;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import ATLUtils.ModelTransformation;
+import running.*;
+
+class TransformationRunnerTests {
+
+	@Test
+	void nonExisitingTrafoDir() {
+		TransformationRunner tool1= new TransformationRunner("izan");
+		boolean exist= tool1.isTrafoDirExist();
+		assertEquals(false, exist, "Trafo folder does not exist");
+	}
+	
+	@Test
+	void exisitingTrafoDir() {
+		TransformationRunner tool1= new TransformationRunner("trafosTest");
+		boolean exist= tool1.isTrafoDirExist();
+		assertEquals(true, exist, "Trafo folder exist");
+	}
+
+	@Test
+	void trafosTestContains2MLs() {
+		TransformationRunner trRun= new TransformationRunner("trafosTest");
+		ArrayList<ModelTransformation> MTsList= trRun.getModelTransformations();
+		assertEquals(2, MTsList.size());
+	}
+	
+	@Test
+	void trafosTestContains3Tools() {
+		TransformationRunner trRun= new TransformationRunner("trafosTest");
+		ArrayList<String> toolsList= trRun.getToolsList();
+		assertEquals(3, toolsList.size());
+		//System.out.println(trRun.printAllMT());
+	}
+	
+	@Test
+	void runTheFolder() {
+		TransformationRunner trRun= new TransformationRunner("trafosTest");
+		boolean run=trRun.runAllTransformations();
+		System.out.println(trRun.getVerbose());
+		assertEquals(true, run);
+	} 
+}
+
