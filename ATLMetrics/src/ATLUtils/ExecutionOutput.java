845ce71b7668a59c97a8496db15f2ee448d0273b
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-08-21 10:11:31 +0200

    Add ExecutionOutput class for easily recording log and execution results

---
+++ ATLMetrics/src/ATLUtils/ExecutionOutput.java
@@ -0,0 +1,18 @@
+package ATLUtils;
+
+public class ExecutionOutput {
+
+	private String log;
+	
+	public ExecutionOutput() {
+		
+	}
+
+	public String getLog() {
+		return log;
+	}
+
+	public void setLog(String log) {
+		this.log = log;
+	}		
+}
