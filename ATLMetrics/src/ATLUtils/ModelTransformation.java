2a59d2c56787289f570e8b7fcb15ee053e756a74
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-08-24 14:30:06 +0200

    Add readATLfile and generateRulesScores methods

--- ATLMetrics/src/ATLUtils/ModelTransformation.java
+++ ATLMetrics/src/ATLUtils/ModelTransformation.java
@@ -1,8 +1,23 @@
 package ATLUtils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.m2m.atl.common.ATL.InPattern;
+import org.eclipse.m2m.atl.common.ATL.InPatternElement;
+import org.eclipse.m2m.atl.common.ATL.MatchedRule;
+import org.eclipse.m2m.atl.common.ATL.Module;
+import org.eclipse.m2m.atl.common.ATL.ModuleElement;
+import org.eclipse.m2m.atl.common.ATL.OutPatternElement;
+import org.eclipse.m2m.atl.common.OCL.OclExpression;
+import org.eclipse.m2m.atl.core.ATLCoreException;
 import org.eclipse.m2m.atl.emftvm.Rule;
+import org.eclipse.m2m.atl.engine.parser.AtlParser;
 
 /**
  * This class creates an object of type Model Transformation. 
@@ -15,6 +30,7 @@ import org.eclipse.m2m.atl.emftvm.Rule;
 public class ModelTransformation {
 	private String name;
 	private String module;
+	private String absoluteATLFilePath;
 	private String inMM;
 	private String inMMRelativePath;
 	private String outMM;
@@ -34,6 +50,80 @@ public class ModelTransformation {
 		rules= new ArrayList<MyRule>();
 		tools= new ArrayList<String>();
 		maxScore=0;
+	}
+	
+	/**
+	 * This method parses an .atl file into a EMF model Eobject.
+	 * 
+	 * @return the main module of the model transformation
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws ATLCoreException
+	 */
+	private Module readATLFile() throws FileNotFoundException, IOException, ATLCoreException {
+		
+		String atlFile="trafosTest/HSM2FSM/HSM2FSM.atl";
+		Module m = null;
+		
+		try (InputStream in = new FileInputStream(atlFile)) {
+		
+		    EObject ast = AtlParser.getDefault().parse(in);
+		    if(ast instanceof Module)
+		    	m=(Module) ast;
+		}
+		
+		return m;
+	}
+		
+	/**
+	 * This method reads all the rules of a model transformation 
+	 * and generates a complexity score for each of them
+	 * Then its add them to this.rules
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws ATLCoreException
+	 */
+	public void createAllRulesScores() throws FileNotFoundException, IOException, ATLCoreException {
+		
+		Module root= readATLFile();
+		
+		if(root instanceof Module){
+			Module mod = (Module) root;
+			for(ModuleElement elem : mod.getElements()){
+				if(elem instanceof MatchedRule){
+					MatchedRule r = (MatchedRule) elem;	
+					int filter=0;
+										
+					InPattern inp= r.getInPattern();
+					List<InPatternElement> ipes= r.getInPattern().getElements();
+					List<OutPatternElement> opes= r.getOutPattern().getElements();
+					if(inp.getFilter()!=null)
+						filter=1;
+					
+					OclExpression ocl= inp.getFilter();
+					
+					int score=1+filter+ipes.size()-1+opes.size()-1;
+					
+					System.out.println(r.getName());
+					System.out.println("  guard:"+filter);
+				    System.out.println("  inVars:"+ipes.size());
+				    System.out.println("  outVars:"+opes.size());
+				    System.out.println("  SCORE="+score);
+				    
+					MyRule rule= new MyRule(r.getName(), score);
+					addRule(rule);											
+				}
+			}
+		}
+	}
+	
+	public String getAbsoluteATLFilePath() {
+		return absoluteATLFilePath;
+	}
+
+	public void setAbsoluteATLFilePath(String absoluteATLFilePath) {
+		this.absoluteATLFilePath = absoluteATLFilePath;
 	}
 	
 	public void addTool(String tool) {
@@ -108,6 +198,7 @@ public class ModelTransformation {
 		String res="";
 		
 		res=res+this.name+" [ module:"+ this.module+", "
+				+ "maxScore:"+ this.maxScore+", "
 				+ "meta-models: "+ this.inMM +"("+this.inMMRelativePath+") > "
 				+ this.outMM+"("+this.outMMRelativePath+") ]\n";
 		
